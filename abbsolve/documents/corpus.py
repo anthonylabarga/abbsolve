610fa4eb48988d68c8977d84b203ff694be1b0d8
John Anthony Labarga <anthony@anthonylabarga.us>
2025-09-09 20:48:09 -0400

    first version of pdf text generator

--- abbsolve/documents/corpus.py
+++ abbsolve/documents/corpus.py
@@ -5,6 +5,8 @@ A corpus generator for reading documents from a directory.
 import os
 from typing import Generator
 
+from pypdf import PdfReader
+
 
 def txt_generator(corpus_dir: str) -> Generator[str, None, None]:
     """
@@ -27,3 +29,28 @@ def txt_generator(corpus_dir: str) -> Generator[str, None, None]:
                 except Exception as e:
                     print(f"Error reading file {file_path}: {e}")
 
+
+def pdf_generator(corpus_dir: str) -> Generator[str, None, None]:
+    """
+    A file handler that reads and yields the content of all .pdf files in a directory and its subdirectories. This
+    function will generally only work with "digitally native" PDFs with embedded text, and not scanned documents.
+
+    Args:
+        corpus_dir: a directory containing .pdf files, possibly nested in multiple subdirectories of arbitrary depth
+
+    Yields:
+        content of each .pdf file as a string
+
+    """
+    for root, _, files in os.walk(corpus_dir):
+        for file in files:
+            if file.endswith(".pdf"):
+                file_path = os.path.join(root, file)
+                try:
+                    reader = PdfReader(file_path)
+                    text = ""
+                    for page in reader.pages:
+                        text += page.extract_text() or ""
+                    yield text
+                except Exception as e:
+                    print(f"Error reading PDF file {file_path}: {e}")
